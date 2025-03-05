5c9e356ce91dd35c224887ca7727580ddfa6ec3e
Jyle Vergara <jylevergara@gmail.com>
2025-03-05 09:15:16 -0500

    remove text

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -18,7 +18,6 @@ export default function TabTwoScreen() {
       <ThemedText>This app includes example code to help you get started.</ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
-          This app has two screens:{' '}
           <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
         </ThemedText>
         <ThemedText>
