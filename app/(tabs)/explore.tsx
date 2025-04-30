b2d8bb914a6aadde008a0724388e561f2fa29336
Jyle Vergara <jylevergara@gmail.com>
2025-04-30 08:56:21 -0400

    remove description

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -35,8 +35,8 @@ export default function TabTwoScreen() {
       </Collapsible>
       <Collapsible title="Images">
         <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
+          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and 
+          suffixes to provide files for
           different screen densities
         </ThemedText>
         <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
