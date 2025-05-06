c48c700ea3ddd4add82e3f2e6ddb6b25973e0b4b
Jyle Vergara <jylevergara@gmail.com>
2025-05-06 15:28:15 -0400

    remove image

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -39,7 +39,6 @@ export default function TabTwoScreen() {
           suffixes to provide files for
           different screen densities
         </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
         <ExternalLink href="https://reactnative.dev">
           <ThemedText type="link">Learn more</ThemedText>
         </ExternalLink>
