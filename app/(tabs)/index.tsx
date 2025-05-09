c86b57e313da04594b2318d9b4ced1da4d4c8052
Jyle Vergara <jylevergara@gmail.com>
2025-05-09 08:52:33 -0400

    remove sample text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -38,7 +38,6 @@ export default function HomeScreen() {
           When you're ready, run{' '}
           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
