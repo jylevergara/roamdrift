9b4001d71eb1014b2f604844132f61cfe56695ff
Jyle Vergara <jylevergara@gmail.com>
2025-05-12 14:22:48 -0400

    remove last text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -35,8 +35,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+          When you're ready, run
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
