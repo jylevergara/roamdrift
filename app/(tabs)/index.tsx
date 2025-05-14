a913752dec494b5af3d96e99d84269c8c058aa01
Jyle Vergara <jylevergara@gmail.com>
2025-05-14 11:52:32 -0400

    remove style

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,7 +15,7 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
+      <ThemedView>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
