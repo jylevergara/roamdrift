7a9765d1de5b9197ef86ceafb0857686b7319a71
Jyle Vergara <jylevergara@gmail.com>
2025-05-14 13:02:44 -0400

    remove title container

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -37,11 +37,6 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
