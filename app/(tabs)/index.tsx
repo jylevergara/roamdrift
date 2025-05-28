2847089d14421e52c6a2c99a596bafcac992c1b4
Jyle Vergara <jylevergara@gmail.com>
2025-05-28 09:13:19 -0400

    remove imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,5 @@
 import { Image } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
