1f081ea9548615628da40024e770d30ad9a7c5d0
Jyle Vergara <jylevergara@gmail.com>
2025-05-31 10:09:22 -0400

    remove style

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -11,9 +11,6 @@ export default function TabTwoScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
@@ -39,9 +36,5 @@ const styles = StyleSheet.create({
     color: '#808080',
     bottom: -90,
     left: -35,
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
   },
 });
