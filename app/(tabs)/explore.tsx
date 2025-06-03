fbd7a393eade2842ddd09dcf7dc594d6b9b156a0
Jyle Vergara <jylevergara@gmail.com>
2025-06-03 11:18:22 -0400

    remove elements

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -4,15 +4,6 @@ import { Collapsible } from '@/components/Collapsible';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 
-export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-    </ParallaxScrollView>
-  );
-}
 
 const styles = StyleSheet.create({
   headerImage: {
