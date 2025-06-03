ec2b57a40f630cae145118acf23bd835e21ee238
Jyle Vergara <jylevergara@gmail.com>
2025-06-03 10:31:22 -0400

    remove collapsible

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -10,11 +10,6 @@ export default function TabTwoScreen() {
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-        </ThemedText>
-      </Collapsible>
     </ParallaxScrollView>
   );
 }
