3a1876df8fce2b7043f496c077d1891bd4e75a2e
Jyle Vergara <jylevergara@gmail.com>
2025-07-08 13:46:09 -0400

    underscore layout file deleted

--- app/(tabs)/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-}
