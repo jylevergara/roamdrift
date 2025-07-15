ec9ffe34336849fe64fa945d93201d3ff7c19a79
Jyle Vergara <jylevergara@gmail.com>
2025-07-15 10:01:01 -0400

    connect color themes

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -5,17 +5,6 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
 }
 
 const styles = StyleSheet.create({
