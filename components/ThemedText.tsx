001dd0a2522b6be56d75ca48b445ea47a39f3544
Jyle Vergara <jylevergara@gmail.com>
2025-07-31 13:09:20 -0400

    add theme colors

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -15,20 +15,6 @@ export function ThemedText({
 }: ThemedTextProps) {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-  return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
 }
 
 const styles = StyleSheet.create({
