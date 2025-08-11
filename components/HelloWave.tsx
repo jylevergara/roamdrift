a3f794b4122df3de159b03b8415467250cfad6e6
Jyle Vergara <jylevergara@gmail.com>
2025-08-11 09:09:11 -0400

    add animated imports

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -1,11 +1,4 @@
 import { StyleSheet } from 'react-native';
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
-  withRepeat,
-  withSequence,
-} from 'react-native-reanimated';
 
 import { ThemedText } from '@/components/ThemedText';
 
