8a174a0e647338d991cb6b0ce5615171b48e9e22
Jyle Vergara <jylevergara@gmail.com>
2025-08-11 09:29:44 -0400

    add rotation animation value

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -5,11 +5,6 @@ import { ThemedText } from '@/components/ThemedText';
 export function HelloWave() {
   const rotationAnimation = useSharedValue(0);
 
-  rotationAnimation.value = withRepeat(
-    withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-    4 // Run the animation 4 times
-  );
-
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{ rotate: `${rotationAnimation.value}deg` }],
   }));
