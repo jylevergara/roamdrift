506cd3030420f63651fdacf8ee92e06755007593
Jyle Vergara <jylevergara@gmail.com>
2025-08-14 10:03:16 -0400

    hellowave return function

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -5,10 +5,5 @@ import { ThemedText } from '@/components/ThemedText';
 export function HelloWave() {
   const rotationAnimation = useSharedValue(0);
 
-  return (
-    <Animated.View style={animatedStyle}>
-      <ThemedText style={styles.text}>👋</ThemedText>
-    </Animated.View>
-  );
 }
 
