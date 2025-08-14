0f611119961e2e161476de2ec591e3dec5726a49
Jyle Vergara <jylevergara@gmail.com>
2025-08-14 11:30:04 -0400

    hellowave component

--- components/HelloWave.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-
-export function HelloWave() {
-  const rotationAnimation = useSharedValue(0);
-
-}
-
